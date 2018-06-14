ab676cd75ded85882a601e38feb857060367bf0b
Laicc1 <carolynclai@gmail.com>
2018-06-14 06:55:10 -0700

    Shader now renders the texture to canvas

--- cel_shading_webgl.js
+++ cel_shading_webgl.js
@@ -14,6 +14,58 @@ function initGL(canvas) {
     gl = WebGLDebugUtils.makeDebugContext(gl, throwOnGLError, validateNoneOfTheArgsAreUndefined);
 }
 
+/*
+ * Initializing the framebuffer and texture
+ */
+var textureBuffer;
+var celTexture;
+var depthTexture;
+
+function initTextureBuffers() {
+
+  var textureWidth = gl.viewportWidth;
+  var textureHeight= gl.viewportHeight;
+  celTexture = gl.createTexture();
+  gl.bindTexture(gl.TEXTURE_2D, celTexture);
+
+  const level = 0;
+  const internalFormat = gl.RGBA;
+  const border = 0;
+  const format = gl.RGBA;
+  const type = gl.UNSIGNED_BYTE;
+  const data = null;
+  gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                textureWidth, textureHeight, border,
+                format, type, data);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+
+  // Create and bind the framebuffer //TODO: referenced from webgl tutorial
+  textureBuffer = gl.createFramebuffer();
+  gl.bindFramebuffer(gl.FRAMEBUFFER, textureBuffer);
+  textureBuffer.width = textureWidth;
+  textureBuffer.height = textureHeight;
+
+  //////     TODO: possibly remove this     ////////////////////////////////////////////////////////////////
+  var renderBuffer = gl.createRenderbuffer();
+  gl.bindRenderbuffer(gl.RENDERBUFFER, renderBuffer);
+  gl.renderbufferStorage(gl.RENDERBUFFER, gl.DEPTH_COMPONENT16, textureBuffer.width, textureBuffer.height);
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const attachmentPoint = gl.COLOR_ATTACHMENT0;
+  const attachLevel = 0;
+  gl.framebufferTexture2D(gl.FRAMEBUFFER, attachmentPoint, gl.TEXTURE_2D, celTexture, attachLevel);
+  gl.framebufferRenderbuffer(gl.FRAMEBUFFER, gl.DEPTH_ATTACHMENT, gl.RENDERBUFFER, renderBuffer); //TODO: possibly remove this
+
+  //finished setup, return the binds back to nothing so that there is no unexpected behavior
+  gl.bindTexture(gl.TEXTURE_2D, null);
+  gl.bindRenderbuffer(gl.RENDERBUFFER, null);
+  gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+
+}
+
 
 /*
  * Initializing object geometries
@@ -75,6 +127,20 @@ function getShader(id) {
     gl.compileShader(shader);
 
     return shader;
+}
+
+
+var testProgram;
+function createOutlineShader(vs_id, fs_id) { //TODO: NEW
+
+  var shaderProg = createShaderProg(vs_id, fs_id);
+  shaderProg.positionLocation = gl.getAttribLocation(shaderProg, "a_position");
+  shaderProg.texcoordLocation = gl.getAttribLocation(shaderProg, "a_texCoord");
+  shaderProg.celTexture = gl.getUniformLocation(shaderProg, "uCelTexture");
+  shaderProg.depthTexture = gl.getUniformLocation(shaderProg, "uDepthTexture");
+
+  return shaderProg;
+
 }
 
 /*
@@ -146,6 +212,9 @@ function initShaders() {
     lightProgram.vertexPositionAttribute = gl.getAttribLocation(lightProgram, "aVertexPosition");
     gl.enableVertexAttribArray(lightProgram.vertexPositionAttribute);
     lightProgram.pMatrixUniform = gl.getUniformLocation(lightProgram, "uPMatrix");
+
+    //TODO: NEW
+    testProgram = createOutlineShader("vs-outline-on", "fs-outline-on");
 }
 
 
@@ -159,6 +228,71 @@ function initBuffers() {
     outlineBuffer = gl.createBuffer();
 }
 
+
+//TODO: function referenced from webgl fundamentals
+function setRectangle(gl, x, y, width, height) {
+  var x1 = x;
+  var x2 = x + width;
+  var y1 = y;
+  var y2 = y + height;
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+     x1, y1,
+     x2, y1,
+     x1, y2,
+     x1, y2,
+     x2, y1,
+     x2, y2,
+  ]), gl.STATIC_DRAW);
+}
+
+function drawBufferToCanvas() {
+  // Create a buffer to put three 2d clip space points in
+  var positionBuffer = gl.createBuffer();
+
+  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // Set a rectangle the same size as the image.
+  setRectangle(gl, 0, 0, textureBuffer.width, textureBuffer.height);
+
+  // provide texture coordinates for the rectangle.
+  var texcoordBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
+    0.0,  0.0,
+    1.0,  0.0,
+    0.0,  1.0,
+    0.0,  1.0,
+    1.0,  0.0,
+    1.0,  1.0,
+  ]), gl.STATIC_DRAW);
+
+  //TODO: skipped resolution stuff for now..
+
+  // Tell WebGL how to convert from clip space to pixels
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  gl.useProgram(testProgram);
+
+  // Turn on the position attribute
+  gl.enableVertexAttribArray(testProgram.positionLocation);
+
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.vertexAttribPointer(testProgram.positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+    // Turn on the texcoord attribute
+  gl.enableVertexAttribArray(testProgram.texcoordLocation);
+
+  // Bind the position buffer.
+  gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+  gl.vertexAttribPointer(testProgram.texcoordLocation, 2, gl.FLOAT, false, 0, 0);
+
+  // lookup uniforms
+  var resolutionLocation = gl.getUniformLocation(testProgram, "u_resolution");
+  gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
+
+}
 
 /*
  * Main rendering code
@@ -190,6 +324,9 @@ var draw_light = false;
 
 
 function drawScene() {
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, textureBuffer); //TODO: THIS LINE WAS ADDED
+
     gl.clear( gl.COLOR_BUFFER_BIT |  gl.DEPTH_BUFFER_BIT);
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 
@@ -216,6 +353,9 @@ function drawScene() {
 
       gl.useProgram(outlineProgram);
 
+      gl.enableVertexAttribArray(outlineProgram.vertexPositionAttribute);
+      gl.enableVertexAttribArray(outlineProgram.vertexNormalAttribute);
+
       gl.uniformMatrix4fv(outlineProgram.pMatrixUniform, false, pMatrix);
       gl.uniformMatrix4fv(outlineProgram.mvMatrixUniform, false, mvMatrix);
       var nMatrix = mat4.transpose(mat4.inverse(mvMatrix));
@@ -235,14 +375,17 @@ function drawScene() {
 
       gl.drawElements(gl.TRIANGLES, currentMesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 
-      // gl.disableVertexAttribArray(outlineProgram.vertexPositionAttribute);
-      // gl.disableVertexAttribArray(outlineProgram.vertexNormalAttribute);
+      gl.disableVertexAttribArray(outlineProgram.vertexPositionAttribute);
+      gl.disableVertexAttribArray(outlineProgram.vertexNormalAttribute);
 
       gl.disable(gl.CULL_FACE);
     }
 
     // Cel Shading Uniforms
     gl.useProgram(currentProgram);
+
+    gl.enableVertexAttribArray(currentProgram.vertexPositionAttribute);
+    gl.enableVertexAttribArray(currentProgram.vertexNormalAttribute);
 
     gl.uniformMatrix4fv(currentProgram.pMatrixUniform, false, pMatrix);
     gl.uniformMatrix4fv(currentProgram.mvMatrixUniform, false, mvMatrix);
@@ -265,6 +408,9 @@ function drawScene() {
 
     gl.drawElements(gl.TRIANGLES, currentMesh.indexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
 
+    gl.disableVertexAttribArray(currentProgram.vertexPositionAttribute);
+    gl.disableVertexAttribArray(currentProgram.vertexNormalAttribute);
+
 
     if (draw_light) {
       gl.useProgram(lightProgram);
@@ -275,6 +421,12 @@ function drawScene() {
       gl.vertexAttribPointer(lightProgram.vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
       gl.drawArrays(gl.POINTS, 0, 1);
     }
+
+
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+    gl.bindTexture(gl.TEXTURE_2D, celTexture);
+
+    drawBufferToCanvas();
 }
 
 var lastTime = 0;
@@ -300,6 +452,7 @@ function webGLStart() {
     var canvas = $("#canvas0")[0];
 
     initGL(canvas);
+    initTextureBuffers(); //TODO: this is new
     initMesh();
     initShaders();
     initBuffers();
